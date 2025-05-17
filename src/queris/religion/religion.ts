af74c6b629666cad59dcd49bae2889357aed3fdf
Sk.Lackmul Haque(Nazmul) <lackmulbe@gmail.com>
2025-05-17 15:57:15 +0600

    reusable pagination for data table

--- src/queris/religion/religion.ts
+++ src/queris/religion/religion.ts
@@ -1,11 +1,19 @@
 import { authAxios } from "@/lib/axios"
 import { useQuery } from "@tanstack/react-query"
 
-export const useReligionQuery = () => {
+type UseReligionQueryParams = {
+    page?: number,
+    page_size?: number;
+    sort_by?: string;
+    sort_type?: string;
+    limit?: number;
+    search?: string;
+}
+export const useReligionQuery = (params: UseReligionQueryParams) => {
     return useQuery({
-        queryKey: ["religions"],
+        queryKey: ["religions", params],
         queryFn: async () => {
-            const res = await authAxios.get("/religions")
+            const res = await authAxios.get("/religions", { params });
             console.log('religions data in useRligionsQuery', res.data);
             return res.data
         },
